c43999ecac3c663758666b5bde5bc60a894e48e3
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-07-05 10:42:02 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BucketList::Application.config.secret_token = '4dae4a2a6644f221e4fac263ca3c29f9147f613bf6f71ace32e26e2b21b502644994c650b40b3df2719b7f7f82129e5563d84d00b56ac2610da1815d52d49108'
